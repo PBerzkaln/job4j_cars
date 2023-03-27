fd244e0fe036968bc749a2e762e643d2c09b382c
pavelberzkaln <p.berzkaln@gmail.com>
2023-03-27 22:46:22 +0300

    2. ManyToMany [#331986]

--- db/scripts/003_ddl_create_price_history_table.sql
+++ db/scripts/003_ddl_create_price_history_table.sql
@@ -1,4 +1,4 @@
-CREATE TABLE price_history
+CREATE TABLE if not exists price_history
 (
     id      SERIAL PRIMARY KEY,
     before  BIGINT not null,
---
+++ db/scripts/004_ddl_create_participates_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE if not exists participates
+(
+    id      serial PRIMARY KEY,
+    post_id int not null REFERENCES auto_post (id),
+    user_id int not null REFERENCES auto_user (id)
+);
